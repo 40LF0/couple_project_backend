0b3a4a2a15d5a9762816089823174709f8c49b58
Lunabell03 <sunnism03@gmail.com>
2024-01-01 23:52:30 +0900

    [feat] ERD 적용

---
+++ src/main/java/com/example/spring/domain/coupon.java
@@ -0,0 +1,24 @@
+package com.example.spring.domain;
+
+import com.example.spring.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class coupon extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private member member;
+}

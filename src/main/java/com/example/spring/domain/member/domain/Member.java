77359e5fb6196ad91ae8324788836c263bb81ceb
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2024-01-19 11:47:14 +0900

    fix: Member 엔티티 수정

--- src/main/java/com/example/spring/domain/member/domain/Member.java
+++ src/main/java/com/example/spring/domain/member/domain/Member.java
@@ -1,7 +1,9 @@
 package com.example.spring.domain.member.domain;
 
+import com.example.spring.domain.auth.domain.Authority;
 import com.example.spring.domain.dateSpot.domain.Recommendation;
 import com.example.spring.domain.review.domain.Review;
+import com.example.spring.domain.user.domain.Role;
 import com.example.spring.global.baseEntity.BaseEntity;
 import com.example.spring.domain.member.enums.Gender;
 import com.example.spring.domain.member.enums.Status;
@@ -11,6 +13,7 @@ import lombok.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Builder
@@ -32,10 +35,26 @@ public class Member extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
+    private String providerId;
+
+    private String provider;
+
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
     @Enumerated(EnumType.STRING)
     private Status status;
 
     private LocalDate inactiveDate;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_authority",
+            joinColumns = {@JoinColumn(name = "member_id", referencedColumnName = "member_id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
+    private Set<Authority> authorities;
 
     @OneToMany (mappedBy = "member", cascade = CascadeType.ALL)
     private List<MemberAgree> memberAgreeList = new ArrayList<>();
@@ -54,4 +73,12 @@ public class Member extends BaseEntity {
 
     @OneToMany (mappedBy = "member", cascade = CascadeType.ALL)
     private List<Qna> qnaList = new ArrayList<>();
+
+    public void updateName(String name){
+        this.name = name;
+    }
+
+    public void updateProfileImage(String imageUrl){
+        this.profileUrl = imageUrl;
+    }
 }

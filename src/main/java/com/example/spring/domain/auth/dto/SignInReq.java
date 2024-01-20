ecd49c97986e0d7e05ea06cde473e5cf13556e71
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2024-01-20 17:43:50 +0900

    fix: dto 수정

--- src/main/java/com/example/spring/domain/auth/dto/SignInReq.java
+++ src/main/java/com/example/spring/domain/auth/dto/SignInReq.java
@@ -1,7 +1,11 @@
 package com.example.spring.domain.auth.dto;
 
+import com.example.spring.domain.member.domain.Member;
+import com.example.spring.domain.member.enums.Role;
 import jakarta.validation.constraints.Email;
 import lombok.*;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Data
 public class SignInReq {
@@ -11,4 +15,15 @@ public class SignInReq {
 
     private String providerId;
 
+    public Member toMember(PasswordEncoder passwordEncoder) {
+        return Member.builder()
+                .email(email)
+                .providerId(passwordEncoder.encode(providerId))
+                .role(Role.USER)
+                .build();
+    }
+
+    public UsernamePasswordAuthenticationToken toAuthentication() {
+        return new UsernamePasswordAuthenticationToken(email, providerId);
+    }
 }
--- src/main/java/com/example/spring/domain/auth/dto/SignUpReq.java
+++ src/main/java/com/example/spring/domain/auth/dto/SignUpReq.java
@@ -1,18 +1,36 @@
 package com.example.spring.domain.auth.dto;
 
+import com.example.spring.domain.member.domain.Member;
+import com.example.spring.domain.member.enums.Role;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Data
 public class SignUpReq {
-
+    @NotBlank
     private String providerId;
-
+    @NotBlank
     private String nickname;
-
+    @NotBlank
     @Email
     private String email;
-
+    @NotBlank
     private String profileImgUrl;
+
+    public Member toMember(PasswordEncoder passwordEncoder) {
+        return Member.builder()
+                .email(email)
+                .providerId(providerId)
+                .password(passwordEncoder.encode(providerId))
+                .role(Role.USER)
+                .build();
+    }
+
+    public UsernamePasswordAuthenticationToken toAuthentication() {
+        return new UsernamePasswordAuthenticationToken(email, providerId);
+    }
 
 }

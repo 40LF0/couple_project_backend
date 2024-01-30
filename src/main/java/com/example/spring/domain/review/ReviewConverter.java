c07bfc90d85d1c4d88eb17c35046e66f14fe462a
SeungYeon <wjdac66@gmail.com>
2024-01-30 20:40:19 +0900

    [feat] ReviewConverter 도입

---
+++ src/main/java/com/example/spring/domain/review/ReviewConverter.java
@@ -0,0 +1,60 @@
+package com.example.spring.domain.review;
+
+import com.example.spring.domain.member.MemberService;
+import com.example.spring.domain.member.domain.Member;
+import com.example.spring.domain.review.domain.Review;
+import com.example.spring.domain.review.dto.ReviewRequestDTO;
+import com.example.spring.domain.review.dto.ReviewResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ReviewConverter {
+    private final MemberService memberService;
+    public ReviewResponseDTO.ReviewEntityDTO toReviewEntityDTO(Review review) {
+        return ReviewResponseDTO.ReviewEntityDTO
+                .builder()
+                .reviewId(review.getReviewId())
+                .title(review.getTitle())
+                .body(review.getBody())
+                .heart(review.getHeart())
+                .imageUrlList(review.fetchImageUrlList())
+                .spotList(review.fetchSpotList())
+                .createdAt(review.getCreatedAt())
+                .build();
+    }
+
+    public Review toReviewEntity(ReviewRequestDTO.ReviewDTO request) {
+        Member member = memberService.findById(request.getMemberId());
+        Review review = Review.builder()
+                .title(request.getTitle())
+                .body(request.getBody())
+                .member(member)
+                .build();
+        review.updateReviewImages(request.getImageUrlList());
+        review.updateSpots(request.getSpotList());
+        return review;
+    }
+    public Page<ReviewResponseDTO.PreviewDTO> toPreviewListDto(Page<Review> reviews) {
+        return reviews.map(review -> {
+            // Extract the first image URL or null if the list is empty
+            String imageUrl = review.getReviewImageList().getFirst().getImgUrl();
+
+            // Extract the member ID
+            Long memberId = review.getMember() != null ? review.getMember().getMemberId() : null;
+
+            // Create and return the PreviewDTO
+            return ReviewResponseDTO.PreviewDTO.builder()
+                    .reviewId(review.getReviewId())
+                    .memberId(memberId)
+                    .title(review.getTitle())
+                    .heart(review.getHeart())
+                    .spotList(review.fetchSpotList())
+                    .imageUrl(imageUrl)
+                    .createdAt(review.getCreatedAt())
+                    .build();
+        });
+    }
+}

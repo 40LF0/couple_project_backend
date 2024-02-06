3c4ac30db6a76f556eedf8e51ff3d887bba760e0
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2024-02-06 19:01:28 +0900

    fix: Member 엔티티 Builder.Default 추가

--- src/main/java/com/example/spring/domain/member/domain/Member.java
+++ src/main/java/com/example/spring/domain/member/domain/Member.java
@@ -25,9 +25,12 @@ public class Member extends BaseEntity {
     private Long memberId;
 
     private String name;
+
     private String profileUrl;
 
+    @Builder.Default
     private int point = 0;
+    @Builder.Default
     private int coupon = 0;
 
     @Enumerated(EnumType.STRING)
@@ -42,8 +45,9 @@ public class Member extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    @Builder.Default
     @Enumerated(EnumType.STRING)
-    private Status status;
+    private Status status = Status.ACTIVE;
 
     private LocalDate inactiveDate;
 

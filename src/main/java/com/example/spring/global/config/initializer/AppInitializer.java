a7202a686f3ae0e072941e777346157c248ce81e
SeungYeon <wjdac66@gmail.com>
2024-02-13 00:02:54 +0900

    [feat] spot DB 저장 로직을 애플리케이션 시작할때 자동으로 실행

---
+++ src/main/java/com/example/spring/global/config/initializer/AppInitializer.java
@@ -0,0 +1,41 @@
+package com.example.spring.global.config.initializer;
+
+import com.example.spring.domain.spotUpdater.SpotUpdaterService;
+import com.example.spring.domain.spotUpdater.enums.SpotArea;
+import com.example.spring.domain.spotUpdater.enums.SpotType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@Configuration
+@RequiredArgsConstructor
+public class AppInitializer {
+
+    @Autowired
+    private final SpotUpdaterService spotUpdaterService;
+
+    @Bean
+    CommandLineRunner initSpotInformation() {
+        return args -> {
+            List<SpotArea> spotAreaList = SpotArea.getAll();
+            List<SpotType> spotTypeList = SpotType.getAll();
+
+            // 모든 SpotArea와 SpotType 조합에 대해 병렬로 updateSpot을 실행합니다.
+            List<CompletableFuture<Void>> futures = spotAreaList.parallelStream().flatMap(spotArea ->
+                    spotTypeList.stream().map(spotType ->
+                            CompletableFuture.runAsync(() ->
+                                    spotUpdaterService.updateSpot(spotArea.getKey(), spotType.getKey())
+                            )
+                    )
+            ).toList();
+
+            // 모든 CompletableFuture가 완료될 때까지 대기합니다.
+            CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+        };
+    }
+}
